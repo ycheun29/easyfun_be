7606079957f9641f9512cae42495707a887d3153
Karlis00 <113260747+Karlis00@users.noreply.github.com>
2023-03-17 18:33:17 -0400

    EAS-25 Refine Configuration of authentication

--- config/config.js
+++ config/config.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 module.exports = {
   ATLASDB: process.env.ATLASDB,
   LOCALDB: process.env.LOCALDB,
-  SECRET: process.env.SECRETKEY,
+  SECRETKEY: process.env.SECRETKEY,
 };
--- config/local.js
+++ config/local.js
@@ -11,7 +11,7 @@ module.exports = function () {
     "tokencheck",
     new JWTstrategy(
       {
-        secretOrKey: config.SECRET,
+        secretOrKey: config.SECRETKEY,
         jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
       },
       async (token, done) => {
