5a6a0dc88da0355744698f9d631e541382bc52b8
Karlis00 <113260747+Karlis00@users.noreply.github.com>
2023-03-30 21:02:47 -0400

    EAS-77 Code route and controllers for create comment

--- controllers/comment.js
+++ controllers/comment.js
@@ -26,3 +26,38 @@ module.exports.commentList = async function (req, res, next) {
     });
   }
 };
+
+// Adding Comment
+module.exports.processAdd = (req, res, next) => {
+  try {
+    let activityId = req.params.activityId;
+    console.log(activityId);
+    let newItem = Comment({
+      _id: req.body.id,
+      comment: req.body.comment,
+      commenter: req.body.commenter,
+      activity: req.body.activity,
+    });
+
+    Comment.create(newItem, (err, item) => {
+      if (err) {
+        console.log(err);
+        // res.end(err);
+        res.status(400).json({
+          success: false,
+          message: getErrorMessage(err),
+        });
+      } else {
+        // refresh the  list
+        console.log(item);
+        // res.redirect('/');
+        res.status(200).json(item);
+      }
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: getErrorMessage(error),
+    });
+  }
+};
--- routes/comment.js
+++ routes/comment.js
@@ -3,6 +3,13 @@ var router = express.Router();
 
 let commentController = require("../controllers/comment");
 
+// Routers for getting comments
 router.get("/", commentController.commentList);
+
+// Routers for adding a comment
+router.post("/add", commentController.processAdd);
+
+// Response
+
 
 module.exports = router;
